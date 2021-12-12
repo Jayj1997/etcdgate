a6c686b289d7dbd32ec0d18c20029947a44970e3
renhanyu <classhanyu@163.com>
2021-12-12 15:54:00 +0800

    change:ui home

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,120 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <img src="../assets/image/hutao01.jpg" alt="etcdgate" />
+        </v-avatar>
+
+        <div>EtcdGate</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container class="grey lighten-5 main_height" fluid>
+        <v-row no-gutters>
+          <v-col cols="4">
+            <v-card :loading="loading" class="mx-auto" max-width="374">
+              <template slot="progress">
+                <v-progress-linear
+                  color="deep-purple"
+                  height="10"
+                  indeterminate
+                ></v-progress-linear>
+              </template>
+              <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              ></v-img>
+              <v-card-title>Cafe Badilico</v-card-title>
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating
+                    :value="4.5"
+                    color="amber"
+                    dense
+                    half-increments
+                    readonly
+                    size="14"
+                  ></v-rating>
+
+                  <div class="grey--text ms-4">4.5 (413)</div>
+                </v-row>
+
+                <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+                <div>
+                  Small plates, salads & sandwiches - an intimate setting with
+                  12 indoor seats plus patio seating.
+                </div>
+              </v-card-text>
+              <v-divider class="mx-4"></v-divider>
+              <v-card-title>Tonight's availability</v-card-title>
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-purple accent-4 white--text"
+                  column
+                >
+                  <v-chip>5:30PM</v-chip>
+                  <v-chip>7:30PM</v-chip>
+                  <v-chip>8:00PM</v-chip>
+                  <v-chip>9:00PM</v-chip>
+                </v-chip-group>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">
+                  Reserve
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="8">
+            <v-card class="pa-2" tile outlined>
+              <v-parallax
+                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+              ></v-parallax>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: () => ({
+    avatar: "../assets/image/hutao.jpg",
+    drawer: null,
+    links: [
+      ['mdi-inbox-arrow-down', 'Inbox'],
+      ['mdi-send', 'Send'],
+      ['mdi-delete', 'Trash'],
+      ['mdi-alert-octagon', 'Spam'],
+    ],
+    loading: false,
+    selection: 1,
+  }),
+  methods: {
+    reserve () {
+      this.loading = true;
+      setTimeout(() => (this.loading = false), 2000);
+    },
   },
 };
 </script>
